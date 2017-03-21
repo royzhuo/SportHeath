5b628cedca68f48ef8157c052864aec5b6fbe255
zhiyi.zhuo <zhiyi.zhuo@qishon.com>
2017-03-21 09:08:52 +0800

    Login

---
+++ src/main/java/com/beStrong/controller/UserLoginController.java
@@ -0,0 +1,10 @@
+package com.beStrong.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by roy on 2017/3/20.
+ */
+@RestControllerAdvice
+public class UserLoginController {
+}
